b0231838cbc1d7c783bea4bd970db538fd1f008e
Gina <g26delarosa@gmail.com>
2019-01-10 12:58:50 -0800

    Initial Commit

---
+++ Cinema Now/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema Now
+//
+//  Created by Gina De La Rosa on 1/10/19.
+//  Copyright © 2019 Gina De La Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Cinema NowTests/Cinema_NowTests.swift
@@ -0,0 +1,34 @@
+//
+//  Cinema_NowTests.swift
+//  Cinema NowTests
+//
+//  Created by Gina De La Rosa on 1/10/19.
+//  Copyright © 2019 Gina De La Rosa. All rights reserved.
+//
+
+import XCTest
+@testable import Cinema_Now
+
+class Cinema_NowTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
