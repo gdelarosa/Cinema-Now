ff37ac0585ad05f8b0ed4f4b14beab2138d4503d
Gina <g26delarosa@gmail.com>
2019-01-23 11:46:31 -0800

    Minor changes to UI. TBC

--- Cinema Now/Controller/DetailViewController.swift
+++ Cinema Now/Controller/DetailViewController.swift
@@ -60,7 +60,7 @@ class DetailViewController: UIViewController {
         }
         // Movie Backdrop 
         if let bannerPath = movie.backdrop_path {
-            let _ = client.taskForGETImage(ImageKeys.PosterSizes.DETAIL_POSTER, filePath: bannerPath) { (data, error) in
+            let _ = client.taskForGETImage(ImageKeys.PosterSizes.BACK_DROP, filePath: bannerPath) { (data, error) in
                 if let data = data {
                     DispatchQueue.main.async {
                         let image = UIImage(data: data)
@@ -78,7 +78,6 @@ class DetailViewController: UIViewController {
         let cP = self.view.viewWithTag(101) as! CircularProgressView
         cP.setProgressWithAnimation(duration: 0.7, value: 0.2)
         cP.trackColor = UIColor.black
-        cP.progressColor = UIColor.orange
         
         let average = movie.vote_average!
         let one = 1.0...1.9
@@ -94,26 +93,37 @@ class DetailViewController: UIViewController {
         
         if one ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.1)
+            cP.progressColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         } else if two ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.2)
+            cP.progressColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         } else if three ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.3)
+            cP.progressColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         } else if four ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.4)
+            cP.progressColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         } else if five ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.5)
+            cP.progressColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         } else if six ~= average {
            cP.setProgressWithAnimation(duration: 0.7, value: 0.6)
+            cP.progressColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
         } else if seven ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.7)
+            cP.progressColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         } else if eight ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.8)
+            cP.progressColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         } else if nine ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.9)
+            cP.progressColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         } else if ten ~= average {
             cP.setProgressWithAnimation(duration: 0.7, value: 1.0)
+            cP.progressColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         } else {
             cP.setProgressWithAnimation(duration: 0.7, value: 0.0)
+            cP.progressColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         }
     }
     
